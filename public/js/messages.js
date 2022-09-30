e8a6123dfda00fd79fef2de5ba5055e2cffca933
Darosss <Daroing12@gmail.com>
2022-09-30 20:06:36 +0200

    Removed jquery from javascript and optimized js a bit

--- public/js/messages.js
+++ public/js/messages.js
@@ -1,61 +1,61 @@
-$(window).ready(function () {
+document.addEventListener("DOMContentLoaded", function (event) {
   var socket = io({ autoConnect: true });
   let typingTime = 5000;
-  let chatContainers = $(".chat-container");
+  let chatContainers = document.getElementsByClassName("chat-container");
+  let timeoutTyping = undefined,
+    typingInChat = false;
   //Loop through all containers to set adequate sockets to chat rooms
-  chatContainers.each(function () {
-    let sendButton = this.querySelector(".btn-primary"),
-      chatWindow = this.querySelector(".chat-window"),
-      chatForm = this.querySelector(".chat-form"),
-      chatTxtarea = this.querySelector("textarea"),
-      joinButton = this.querySelector('button[id*="join"]'),
-      leaveButton = this.querySelector('button[id*="leave"]'),
-      roomID = "#" + $(this).attr("id"),
-      typingInChat = false,
-      timeoutTyping = undefined;
+  [...chatContainers].forEach((chat) => {
+    let sendButton = chat.querySelector(".btn-primary"),
+      chatWindow = chat.querySelector(".chat-window"),
+      chatForm = chat.querySelector(".chat-form"),
+      chatTxtarea = chat.querySelector("textarea"),
+      joinButton = chat.querySelector('button[id*="join"]'),
+      leaveButton = chat.querySelector('button[id*="leave"]'),
+      roomID = "#" + chat.getAttribute("id");
 
     textareaEventListeners(chatTxtarea, sendButton);
-    dynamicResizeTextarea(chatTxtarea);
     chatWindow.scrollTop = chatWindow.scrollHeight;
-
-    $(joinButton).on("click", function () {
+    // Join appropriate room by this button
+    joinButton.addEventListener("click", function () {
       sendButton.style.display = "inline-block";
       joinLeaveRoom(this, leaveButton, "join room", roomID);
     });
-    $(leaveButton).on("click", function () {
+    // Leave appropriate room by this button
+    leaveButton.addEventListener("click", function () {
       joinLeaveRoom(this, joinButton, "leave room", roomID);
       sendButton.style.display = "none";
     });
-    //Submit form and send msg
-    $(chatForm).on(
-      "submit",
-      { textarea: chatTxtarea, room: roomID },
-      onSubmitMessage
-    );
-    // On key preess textarea(Typing user feature)
-    $(chatTxtarea).on(
-      "keypress",
-      { room: roomID, timeout: timeoutTyping, typing: typingInChat },
-      typingInfoOnKeypress
-    );
+    //When submiting form prevent default and emit 'message' to socket
+    chatForm.addEventListener("submit", function (e) {
+      e.preventDefault();
+      onSubmitMessage(chatTxtarea, roomID);
+    });
+    // On keypress at textarea emit 'typing' to socket
+    chatTxtarea.addEventListener("keypress", function (e) {
+      typingInfoOnKeypress(e, roomID);
+    });
   });
   //END For each chat room
-  function typingInfoOnKeypress(e) {
-    if (e.which != 13) {
-      typingTimeout(e.data.room, (e.data.typing = true));
-      clearTimeout(e.data.timeout);
-      e.data.timeout = setTimeout(
+
+  //Function for 'emit' typing
+  function typingInfoOnKeypress(event, room) {
+    if (event.which != 13) {
+      typingTimeout(room, (typingInChat = true));
+      clearTimeout(timeoutTyping);
+      timeoutTyping = setTimeout(
         typingTimeout,
         typingTime,
-        e.data.room,
-        (e.data.typing = false)
+        room,
+        (typingInChat = false)
       );
     } else {
-      e.data.typing = false;
-      typingTimeout(e.data.room, e.data.typing);
-      clearTimeout(e.data.timeout);
+      typingInChat = false;
+      typingTimeout(room, typingInChat);
+      clearTimeout(timeoutTyping);
     }
   }
+  //Trigger when gets 'chat message'
   socket.on("chat message", function (data) {
     addMessageToChatbox(
       data.username,
@@ -64,7 +64,7 @@ $(window).ready(function () {
       data.date.split("T")[1].split(".")[0]
     );
   });
-
+  //Trigger when someone is typing in your room
   socket.on("user typing", (data) => {
     const chatInfo = document.querySelector(data.roomName + " .chat-info");
     if (!data.isTyping) {
@@ -73,6 +73,7 @@ $(window).ready(function () {
       chatInfo.innerHTML = `${data.username} is typing`;
     }
   });
+  //Trigger when someone is going offline or online in room
   socket.on("user online room", function (data) {
     let roomUserList = document.querySelector(data.roomName + " .user-list");
     roomUserList.innerHTML = "";
@@ -89,36 +90,38 @@ $(window).ready(function () {
   // socket.on("private msg", function (fromUser, msg) {
   //   console.log("PRIV MSG", fromUser, msg);
   // });
+
+  //Function that will disable 'typing' by user
   function typingTimeout(room, typing) {
     socket.emit("user typing", {
       roomName: room,
       isTyping: typing,
     });
   }
+  //Join leave appropriate room
   function joinLeaveRoom(btnPressed, btnToEnable, evnName, roomName) {
     socket.emit(evnName, { roomName: roomName });
     btnPressed.disabled = true;
     $(btnToEnable).removeAttr("disabled");
   }
-  function onSubmitMessage(event) {
-    event.preventDefault();
-    let chatTextarea = event.data.textarea,
-      roomName = event.data.room;
-
-    if (!chatTextarea.value) return;
+  //Emit message to each room and emit
+  function onSubmitMessage(textarea, room) {
+    if (!textarea.value) return;
 
     socket.emit("chat message", {
-      roomTarget: roomName,
-      msg: chatTextarea.value,
+      roomTarget: room,
+      msg: textarea.value,
     });
-    chatTextarea.value = "";
+    textarea.value = "";
   }
+  //When gets message this function paste it to div
   function addMessageToChatbox(username, room, msg, date) {
     var msgBox = messageFormat(username, msg, date);
     roomTarget = document.querySelector(room);
     roomTarget.innerHTML += msgBox;
     roomTarget.scrollTop = roomTarget.scrollHeight;
   }
+  //Message format
   function messageFormat(user, msg, time) {
     let msgBox = `
           <div class="message-div">
@@ -130,32 +133,31 @@ $(window).ready(function () {
           </div>`;
     return msgBox;
   }
-  function dynamicResizeTextarea(textarea) {
-    textarea.setAttribute(
-      "style",
-      "height:" + textarea.scrollHeight + "px;overflow-y:hidden;"
-    );
-  }
+
+  //Textarea listeners fe. (dynamical resizing, enter to send message when focus etc.)
   function textareaEventListeners(textarea, btn) {
     textarea.addEventListener("input", textareaSizeDynamical, false);
     textarea.addEventListener("focus", onFocusEnterSend, false);
     textarea.sendButton = btn;
     textarea.addEventListener("focusout", onFocusOutDisableEnter, false);
   }
+  //Dynamical resize textarea on texting
   function textareaSizeDynamical() {
     this.style.height = 0;
     this.style.height = this.scrollHeight + "px";
   }
+  //Enter to send when focus textarea
   function onFocusEnterSend() {
     this.addEventListener("keypress", enterToSendOnFocus);
-  }
-  function onFocusOutDisableEnter() {
-    this.removeEventListener("keypress", onFocusEnterSend, true);
   }
   function enterToSendOnFocus(e) {
     if ((e.code == "Enter" || e.code == "NumpadEnter") && !e.shiftKey) {
       e.preventDefault();
       e.currentTarget.sendButton.click();
     }
+  }
+  //Disable enter to send when focus out textarea
+  function onFocusOutDisableEnter() {
+    this.removeEventListener("keypress", onFocusEnterSend, true);
   }
 });
