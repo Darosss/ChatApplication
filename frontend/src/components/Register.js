eed445fb6c782e01f5853f5375a3aa9b214ff0cd
Darosss <Daroing12@gmail.com>
2022-10-17 02:11:20 +0200

    style: add styles for register frontend

--- frontend/src/components/Register.js
+++ frontend/src/components/Register.js
@@ -30,7 +30,7 @@ function App() {
         phone: phone,
       },
       withCredentials: true,
-      url: "/register",
+      url: "http://localhost:5000/register",
     }).then((res) => {
       console.log(res);
       navigate("/login");
@@ -41,88 +41,127 @@ function App() {
       <header className="App-header">
         <h2> Register </h2>
         <form onSubmit={register}>
-          <div className="form-group">
-            <label>Username: </label>
-            <input
-              type="text"
-              className="form-control"
-              onChange={(e) => setUsername(e.target.value)}
-            />
+          <div className="row mt-2">
+            <div className="col">
+              <label> Username </label>
+              <input
+                type="text"
+                className="form-control"
+                placeholder="Username"
+                onChange={(e) => setUsername(e.target.value)}
+              />
+            </div>
+            <div className="col">
+              <label> E-mail </label>
+              <input
+                type="text"
+                className="form-control"
+                placeholder="E-mail"
+                onChange={(e) => setEmail(e.target.value)}
+              />
+            </div>
+            <div className="col ">
+              <label> Password </label>
+              <input
+                type="password"
+                className="form-control"
+                placeholder="Password"
+                onChange={(e) => setPassword(e.target.value)}
+              />
+            </div>
           </div>
-          <div className="form-group">
-            <label>Password: </label>
-            <input
-              type="password"
-              className="form-control"
-              onChange={(e) => setPassword(e.target.value)}
-            />
+
+          <div className="row mt-2">
+            <div className="col ">
+              <label> Firstname </label>
+              <input
+                type="text"
+                className="form-control"
+                placeholder="Firstname"
+                onChange={(e) => setFirstname(e.target.value)}
+              />
+            </div>
+            <div className="col ">
+              <label> Surname </label>
+              <input
+                type="text"
+                className="form-control"
+                placeholder="Surname"
+                onChange={(e) => setSurname(e.target.value)}
+              />
+            </div>
           </div>
-          <div className="form-group">
-            <label>Firstname: </label>
-            <input
-              type="text"
-              className="form-control"
-              onChange={(e) => setFirstname(e.target.value)}
-            />
+          <div className="row mt-2">
+            <div className="col ">
+              <label> Birthday </label>
+              <input
+                type="date"
+                className="form-control"
+                placeholder="Birthday"
+                onChange={(e) => setBirthday(e.target.value)}
+              />
+            </div>
+            <div className="col ">
+              <label> Phone </label>
+              <input
+                type="text"
+                className="form-control"
+                placeholder="Phone"
+                onChange={(e) => setPhone(e.target.value)}
+              />
+            </div>
           </div>
-          <div className="form-group">
-            <label>Surname: </label>
-            <input
-              type="text"
-              className="form-control"
-              onChange={(e) => setSurname(e.target.value)}
-            />
+          <div className="row mt-2">
+            <div className="col ">
+              <label> Country </label>
+              <select
+                className="form-control"
+                id="country"
+                onChange={(e) => setCountry(e.target.value)}
+              >
+                <option></option>
+                <option>Poland</option>
+                <option>Germany</option>
+                <option>French</option>
+                <option>USA</option>
+                <option>Spain</option>
+              </select>
+            </div>
+            <div className="col ">
+              <label> Gender </label>
+              <select
+                className="form-control"
+                id="gender"
+                onChange={(e) => setGender(e.target.value)}
+              >
+                <option>Male</option>
+                <option>Female</option>
+                <option>N/A</option>
+              </select>
+            </div>
+            <div className="col ">
+              <label> Nick color </label>
+
+              <select
+                className="form-control"
+                id="nickColor"
+                onChange={(e) => setNickColor(e.target.value)}
+              >
+                <option className="bg-danger">Red</option>
+                <option className="bg-light">White</option>
+                <option className="bg-dark">Black</option>
+                <option className="bg-success">Green</option>
+                <option className="bg-primary">Blue</option>
+              </select>
+            </div>
           </div>
+
           <div className="form-group">
-            <label>Birthday: </label>
             <input
-              type="date"
-              className="form-control"
-              onChange={(e) => setBirthday(e.target.value)}
+              type="submit"
+              value="Register"
+              className="btn btn-primary w-100"
             />
-          </div>
-          <div className="form-group">
-            <label>Country: </label>
-            <input
-              type="text"
-              className="form-control"
-              onChange={(e) => setCountry(e.target.value)}
-            />
-          </div>
-          <div className="form-group">
-            <label>Gender: </label>
-            <input
-              type="text"
-              className="form-control"
-              onChange={(e) => setGender(e.target.value)}
-            />
-          </div>
-          <div className="form-group">
-            <label>Nick color: </label>
-            <input
-              type="text"
-              className="form-control"
-              onChange={(e) => setNickColor(e.target.value)}
-            />
-          </div>
-          <div className="form-group">
-            <label>E-mail: </label>
-            <input
-              type="text"
-              className="form-control"
-              onChange={(e) => setEmail(e.target.value)}
-            />
-          </div>
-          <div className="form-group">
-            <label>Phone: </label>
-            <input
-              type="text"
-              className="form-control"
-              onChange={(e) => setPhone(e.target.value)}
-            />
-          </div>
-          <div className="form-group">
-            <input type="submit" value="Register" className="btn btn-primary" />
           </div>
         </form>
       </header>
