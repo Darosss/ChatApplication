16154f0961bc539f2dee8e3308cf9c8612e79151
Darosss <daroing12@gmail.com>
2022-10-22 17:39:26 +0200

    fix: correct name of partial in require

--- app/routes/middlewares/isAdminForwarding.js
+++
@@ -1,9 +0,0 @@
-const isAdmin = require("../partials/isAdmin");
-module.exports = async function (req, res, next) {
-  let userId = req.session.passport.user._id;
-  if (await isAdmin(userId)) {
-    return next();
-  } else {
-    res.redirect("/");
-  }
-};
