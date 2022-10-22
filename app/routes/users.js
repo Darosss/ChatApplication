d77b8af0b505cbc588dfb4f1a80b2971543e6e62
Darosss <daroing12@gmail.com>
2022-10-22 20:14:46 +0200

    feat!: users route now send json instead of redirect

--- app/routes/users.js
+++ app/routes/users.js
@@ -3,23 +3,26 @@ const router = express.Router();
 const user = require("../models/user");
 const range = require("../models/range");
 const chatRoom = require("../models/chatRoom");
-const dir = "users";
+
 router.get("/", async (req, res) => {
   let users;
   try {
-    users = await user.find({}).populate("ranges");
-  } catch {}
-
-  res.render(dir + "/index", {
-    users: users,
-  });
+    users = await user.find({}, { password: 0, __v: 0 });
+    res.send({ usersList: users });
+  } catch {
+    res.send({ message: "Can't get users" });
+  }
 });
 //Get user by id
-router.get("/edit/:id", async (req, res) => {
-  const userEdit = await user.findById(req.params.id).populate("ranges");
+router.get("/:userId", async (req, res) => {
+  const userEdit = await user.findById(req.params.userId, {
+    password: 0,
+    __v: 0,
+    createdAt: 0,
+  });
   const ranges = await range.find({});
   const chatRooms = await chatRoom.find({ createdBy: userEdit.id });
-  res.render(dir + "/edit", {
+  res.send({
     user: userEdit,
     ranges: ranges,
     chatRooms: chatRooms,
@@ -27,47 +30,44 @@ router.get("/edit/:id", async (req, res) => {
 });
 
 //Edit user by id route
-router.post("/edit/:id", async (req, res) => {
-  const update = {
-    username: req.body.name,
-    ranges: req.body.ranges,
-  };
+router.post("/edit/:userId", async (req, res) => {
   try {
-    await user.findByIdAndUpdate(req.params.id, update);
-    res.redirect("/" + dir);
+    await user.findByIdAndUpdate(req.params.userId, req.body.userDetails);
+    res.send({ message: "User edited" });
   } catch (e) {
+    res.send({ message: "Can't edit user" });
     console.log(e);
   }
 });
-//Ban user by id route
-router.post("/ban/:id", async (req, res) => {
-  let bannedDate = new Date();
-  let banExpiresDate = new Date();
-  let banMinutes = banExpiresDate.getMinutes() + parseInt(req.body.banTime);
-  banExpiresDate.setMinutes(banMinutes);
+// //Ban user by id route
+// router.post("/ban/:id", async (req, res) => {
+//   let bannedDate = new Date();
+//   let banExpiresDate = new Date();
+//   let banMinutes = banExpiresDate.getMinutes() + parseInt(req.body.banTime);
+//   banExpiresDate.setMinutes(banMinutes);
 
-  const update = {
-    isBanned: true,
-    bannedDate: bannedDate,
-    banExpiresDate: banExpiresDate,
-  };
-  try {
-    await user.findByIdAndUpdate(req.params.id, update);
-    res.redirect("/" + dir);
-  } catch (e) {
-    console.log(e);
-  }
-});
-//Unban user by id route
-router.post("/unban/:id", async (req, res) => {
-  const update = {
-    isBanned: false,
-  };
-  try {
-    await user.findByIdAndUpdate(req.params.id, update);
-    res.redirect("/" + dir);
-  } catch (e) {
-    console.log(e);
-  }
-});
+//   const update = {
+//     isBanned: true,
+//     bannedDate: bannedDate,
+//     banExpiresDate: banExpiresDate,
+//   };
+//   try {
+//     await user.findByIdAndUpdate(req.params.userId, update);
+//     res.send({ message: "User banned" });
+//   } catch (e) {
+//     console.log(e);
+//   }
+// });
+// //Unban user by id route
+// router.post("/unban/:id", async (req, res) => {
+//   const update = {
+//     isBanned: false,
+//   };
+//   try {
+//     await user.findByIdAndUpdate(req.params.userId, update);
+//     res.send({ message: "User unbanned" });
+//   } catch (e) {
+//     console.log(e);
+//   }
+// });
 module.exports = router;
