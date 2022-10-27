e10a7dd77f694a387af8e95a2c24bf8be686fc5e
Darosss <daroing12@gmail.com>
2022-10-27 13:36:00 +0200

    refactor: change file to work with ModalCore
    remove unused states, axios and buttons

--- frontend/src/components/rooms/Rooms.js
+++ frontend/src/components/rooms/Rooms.js
@@ -2,10 +2,10 @@ import React, { useEffect, useState } from "react";
 import axios from "axios";
 import UserRoomsList from "./UserRoomsList";
 import EditCreateRoomModal from "./EditCreateRoomModal";
+
 function Rooms() {
   const [rooms, setRooms] = useState([]);
-  const [ranges, setRanges] = useState([]);
-  const [usersList, setUsersList] = useState([]);
+
   useEffect(() => {
     const axiosConfigRooms = {
       method: "get",
@@ -15,15 +15,6 @@ function Rooms() {
     axios(axiosConfigRooms).then((res) => {
       setRooms(res.data.usersRooms);
     });
-    const axiosConfigCreateRoom = {
-      method: "get",
-      withCredentials: true,
-      url: "http://localhost:5000/rooms/create",
-    };
-    axios(axiosConfigCreateRoom).then((res) => {
-      setRanges(res.data.availableRanges);
-      setUsersList(res.data.usersList);
-    });
   }, []);
 
   return (
@@ -31,23 +22,9 @@ function Rooms() {
       <header className="App-header">
         <div className="d-inline-flex">
           <div className="m-1">Your chat rooms</div>
-          <button
-            className="btn btn-primary"
-            data-toggle="modal"
-            data-target="#create-room"
-          >
-            NEW ROOM
-          </button>
+          <EditCreateRoomModal sectionName="Create" />
         </div>
-        {
-          <EditCreateRoomModal
-            id="create-room"
-            sectionName="Create"
-            postSuffix="create"
-            availableRanges={ranges}
-            usersList={usersList}
-          />
-        }
+        {}
         {<UserRoomsList rooms={rooms} />}
       </header>
     </div>
