009f49f185cdfe7ee1082fda3ae898e5c4407e35
Darosss <daroing12@gmail.com>
2022-10-30 00:00:49 +0200

    feat: add prototype socket io client

--- frontend/src/components/ChatRoom.js
+++ frontend/src/components/ChatRoom.js
@@ -1,21 +1,88 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import ChatMessage from "./ChatMessage";
 import Col from "react-bootstrap/Col";
 import Nav from "react-bootstrap/Nav";
 import Row from "react-bootstrap/Row";
 import Tab from "react-bootstrap/Tab";
 import Table from "react-bootstrap/Table";
+import Button from "react-bootstrap/Button";
+
+import io from "socket.io-client";
+const socket = io.connect("http://localhost:5000");
 
 function ChatRoom(props) {
+  const [isConnected, setIsConnected] = useState(socket.connected);
+
+  const [msgToSend, setMsgToSend] = useState("");
+  // const [lastPong, setLastPong] = useState(null);
+
   const chatRooms = props.chatRooms;
   const messages = props.messages;
+  const auth = props.auth;
+  const username = auth.username;
+  const userId = auth._id;
 
   let roomsList = [];
   let roomsContent = [];
 
+  useEffect(() => {
+    socket.on("connect", () => {
+      console.log("Connect");
+
+      setIsConnected(true);
+    });
+
+    socket.on("disconnect", () => {
+      console.log("Disconnect");
+      setIsConnected(false);
+    });
+
+    // socket.on("pong", () => {
+    //   setLastPong(new Date().toISOString());
+    // });
+
+    socket.on("chat message", (data) => {
+      console.log("new message", data);
+    });
+
+    return () => {
+      socket.off("connect");
+      socket.off("disconnect");
+      socket.off("chat message");
+      socket.off("join channels");
+      socket.off("pong");
+    };
+  }, []);
+
+  useEffect(() => {
+    socket.on("join channels", () => {
+      chatRooms.forEach((room) => {
+        let data = { username: username, userId: userId, roomId: room._id };
+
+        socket.emit("join channel", data);
+      });
+    });
+  }, [username, userId, chatRooms]);
+
+  // const sendPing = () => {
+  //   console.log("lol", socket);
+  //   socket.emit("ping");
+  // };
+
+  const sendMessage = (e) => {
+    let data = {
+      roomId: e.target.id,
+      userId: userId,
+      username: username,
+      msg: msgToSend,
+    };
+
+    socket.emit("chat message", data);
+  };
+
   const roomContent = (room) => {
     return (
-      <Table striped bordered hover variant="dark">
+      <Table striped bordered variant="dark">
         <thead>
           <tr>
             <th colSpan={3}>{room.name} </th>
@@ -34,6 +101,17 @@ function ChatRoom(props) {
               <div className="chat-scrollable">Online users</div>
             </td>
           </tr>
+          <tr>
+            <td>
+              <Button
+                className="w-75 btn-secondary"
+                id={room._id}
+                onClick={sendMessage}
+              >
+                Send
+              </Button>
+            </td>
+          </tr>
         </tbody>
       </Table>
     );
@@ -46,7 +124,6 @@ function ChatRoom(props) {
   };
 
   chatRooms.forEach((room) => {
-    console.log("room", room.name);
     roomsList.push(
       <Nav.Item key={room._id} className="w-100">
         <Nav.Link eventKey={room._id}> {room.name} </Nav.Link>
