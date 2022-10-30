34a0670f13c4a74a5a44214ada4dee4ff3ccb57b
Darosss <daroing12@gmail.com>
2022-10-30 13:57:11 +0100

    fix: remove console.logs

--- app/routes/currSessionRouter.js
+++ app/routes/currSessionRouter.js
@@ -2,12 +2,10 @@ const express = require("express");
 const router = express.Router();
 const passport = require("passport");
 router.get("/current-session", (req, res) => {
-  console.log("curre-session");
   passport.authenticate("jwt", { session: false }, (err, user) => {
     if (err || !user) {
       res.send(false);
     } else {
-      console.log("true", user);
       res.send(user);
     }
   })(req, res);
