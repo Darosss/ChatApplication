d8927330cdd3c18ca32fbebe7e6a663abfae3014
Darosss <daroing12@gmail.com>
2022-11-08 23:26:34 +0100

    refactor!: add custom middleware to check if user is logged

--- app/routes/register.js
+++ app/routes/register.js
@@ -2,33 +2,40 @@ const express = require("express");
 const router = express.Router();
 const User = require("../models/user");
 
-router.get("/", (req, res) => {
-  res.send("Register site");
-});
+router.post(
+  "/",
+  (req, res, next) => {
+    if (req.user) res.send("You are already registered");
+    else return next();
+  },
 
-router.post("/", async (req, res) => {
-  console.log(req.body);
-  let userData = {
-    username: req.body.username,
-    password: req.body.password,
-    firstname: req.body.firstname,
-    surname: req.body.surname,
-    birthday: req.body.birthday,
-    country: req.body.country,
-    gender: req.body.gender,
-    nickColor: req.body.nickColor,
-    email: req.body.email,
-    phoneNumber: req.body.phone,
-  };
-  User.findOne({ username: req.body.username }, async (err, doc) => {
-    if (err) throw err;
-    if (doc) res.send("User already exist");
-    if (!doc) {
-      const newUser = new User(userData);
-      await newUser.save();
-    }
-  });
-  res.send("Account created");
-});
+  async (req, res) => {
+    let userData = {
+      username: req.body.username,
+      password: req.body.password,
+      firstname: req.body.firstname,
+      surname: req.body.surname,
+      birthday: req.body.birthday,
+      country: req.body.country,
+      gender: req.body.gender,
+      nickColor: req.body.nickColor,
+      email: req.body.email,
+      phoneNumber: req.body.phone,
+    };
+    User.findOne({ username: req.body.username }, async (err, doc) => {
+      if (err) throw err;
+      if (doc) res.send("User already exist");
+      if (!doc) {
+        const newUser = new User(userData);
+        try {
+          await newUser.save();
+          res.send("Account created");
+        } catch (err) {
+          res.send("Some fields can't be empty");
+        }
+      }
+    });
+  }
+);
 
 module.exports = router;
