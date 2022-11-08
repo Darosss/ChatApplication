552bba9f5ad566733680a2f60f19dc0813c544e3
Darosss <daroing12@gmail.com>
2022-11-08 21:41:37 +0100

    refactor: remove unused files

--- frontend/src/auth/RequireAuthLink.js
+++
@@ -1,8 +0,0 @@
-import React from "react";
-import { useAuth } from "./useAuth";
-
-export const RequireAuthLink = ({ children }) => {
-  const auth = useAuth();
-  console.log("auth link", auth.username);
-  return auth.username ? children : "not ";
-};
