f7756e0927e77c32bc949e0f54328ac58b55336b
Darosss <daroing12@gmail.com>
2022-11-13 00:08:30 +0100

    refactor!: add status code to middleware
    assign await to findById method
    assign toString method to createdBy

--- app/routes/middlewares/chatRoomValidation.js
+++ app/routes/middlewares/chatRoomValidation.js
@@ -2,9 +2,9 @@ const isAdmin = require("../partials/_isAdminUser");
 const chatRoom = require("../../models/chatRoom");
 module.exports = async function (req, res, next) {
   let roomId = req.params.roomId;
-  let room = chatRoom.findById(roomId);
+  let room = await chatRoom.findById(roomId);
   let userId = req.user.id;
-  if ((await room.createdBy) === userId || (await isAdmin(userId)))
+  if (room.createdBy.toString() === userId || (await isAdmin(userId)))
     return next();
-  else res.send({ message: "You are not owner of this room" });
+  else res.status(403).send({ message: "You are not owner of this room" });
 };
