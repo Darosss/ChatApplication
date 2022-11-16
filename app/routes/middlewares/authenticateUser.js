63980f08d2d7dc95a86da7986d0bcdbd71a4ab7b
Darosss <daroing12@gmail.com>
2022-11-16 17:12:50 +0100

    feat: add send message when no user is found
    remove unnecesary else and comments

--- app/routes/middlewares/authenticateUser.js
+++ app/routes/middlewares/authenticateUser.js
@@ -4,25 +4,20 @@ const jwt = require("jsonwebtoken");
 module.exports = function (req, res, next) {
   passport.authenticate("local", (err, user, info) => {
     if (err) throw err;
-    if (!user) return res.redirect("/login");
-    else {
-      req.logIn(user, (er) => {
-        if (err) throw err;
-        if (user) {
-          const userReturnObject = {
-            username: user.username,
-          };
-          req.session.jwt = jwt.sign(
-            userReturnObject,
-            process.env.JWT_SECRET_KEY
-          );
-          res.send({ message: "Logged in!" });
-          // res.status(201).json({
-          //   message: "User successfully Logged in",
-          //   user: user._id,
-          // });
-        }
-      });
-    }
+    if (!user) return res.status(200).send({ message: "Not found user" });
+
+    req.logIn(user, (er) => {
+      if (err) throw err;
+      if (user) {
+        const userReturnObject = {
+          username: user.username,
+        };
+        req.session.jwt = jwt.sign(
+          userReturnObject,
+          process.env.JWT_SECRET_KEY
+        );
+        next();
+      }
+    });
   })(req, res, next);
 };
