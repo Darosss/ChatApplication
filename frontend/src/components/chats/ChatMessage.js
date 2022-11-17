a9661817f1f81882870831206e50d0234a594d7f
Darosss <daroing12@gmail.com>
2022-11-17 20:27:41 +0100

    feat: add styles for chat messages

--- frontend/src/components/chats/ChatMessage.js
+++ frontend/src/components/chats/ChatMessage.js
@@ -4,15 +4,14 @@ function ChatMessage(props) {
   return (
     <tr>
       <td className="w-25">
-        <span
-          className="text-muted message-time fs-6"
-          style={{ fontSize: "2vmin" }}
-        >
+        <span className="chat-message-time">
           {props.sentTime.replace("T", " ").split(".")[0]}:
         </span>
-        <span> {props.sender}</span>
+        <span className="chat-message-sender"> {props.sender}</span>
       </td>
-      <td className="w-75 text-wrap text-break">{props.message}</td>
+      <td className="w-75 text-wrap text-break bg-dark chat-message-msg">
+        {props.message}
+      </td>
     </tr>
   );
 }
