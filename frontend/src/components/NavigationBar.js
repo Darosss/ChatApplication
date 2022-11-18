d50091e7554ff039d2aace043891617899e6aedd
Darosss <daroing12@gmail.com>
2022-11-18 18:43:29 +0100

    feat: add navgiation menu to work with small displays
    add HamburgerIcon to NavigationBar.js
    import useState, useRef, Link
    add toggle function to show/hide menu
    add className to li in NavigationLink
    add css for menu in App.css

--- frontend/src/components/NavigationBar.js
+++ frontend/src/components/NavigationBar.js
@@ -1,21 +1,44 @@
+import React, { useState, useRef, useEffect } from "react";
 import NavigationLink from "./NavigationLink";
 import Logout from "./Logout";
+import { HamburgerIcon } from "react-hamburger-icon";
+import { Link } from "react-router-dom";
 
 function NavigationBar({ auth }) {
+  const [open, setOpen] = useState(false);
+
+  const navigation = useRef();
+
+  const toggleNavigation = () => {
+    navigation.current.classList.toggle("show");
+  };
+
   return (
-    <nav className="navbar navbar-expand-sm navbar-dark bg-dark">
-      <span className="navbar-brand m-2">Chat room</span>
-      <div className="collapse navbar-collapse" id="navbarSupportedContent">
-        <ul className="navbar-nav mr-auto">
-          <NavigationLink url="/" name="Home" />
-          {auth ? <NavigationLink url="/chats" name="Chats" /> : null}
-          {!auth ? <NavigationLink url="/login" name="Login" /> : null}
-          {!auth ? <NavigationLink url="/register" name="Register" /> : null}
-          {auth ? <NavigationLink url="/rooms" name="Rooms" /> : null}
-          {auth ? <NavigationLink url="/profil" name="Profil" /> : null}
-        </ul>
-      </div>
-      <div className="m-2">
+    <nav className="bg-secondary">
+      <Link className="home-site-link" to="/">
+        Chat room
+      </Link>
+
+      <HamburgerIcon
+        className="hamburger"
+        id="hamburger"
+        open={open}
+        onClick={() => {
+          setOpen(!open);
+          toggleNavigation();
+        }}
+      />
+
+      <ul className="nav-ul" id="nav-ul" ref={navigation}>
+        <NavigationLink url="/" name="Home" />
+        {auth ? <NavigationLink url="/chats" name="Chats" /> : null}
+        {!auth ? <NavigationLink url="/login" name="Login" /> : null}
+        {!auth ? <NavigationLink url="/register" name="Register" /> : null}
+        {auth ? <NavigationLink url="/rooms" name="Rooms" /> : null}
+        {auth ? <NavigationLink url="/profil" name="Profil" /> : null}
+      </ul>
+
+      {/* <div className="m-2">
         <ul className="navbar-nav mr-auto">
           {auth && auth.administrator ? (
             <NavigationLink url="/users" name="Users" />
@@ -25,7 +48,7 @@ function NavigationBar({ auth }) {
           ) : null}
           {auth ? <Logout /> : null}
         </ul>
-      </div>
+      </div> */}
     </nav>
   );
 }
--- frontend/src/components/NavigationLink.js
+++ frontend/src/components/NavigationLink.js
@@ -3,8 +3,8 @@ import { Link } from "react-router-dom";
 
 function NavigationLink(props) {
   return (
-    <li className="nav-item">
-      <Link to={props.url} className="nav-link">
+    <li>
+      <Link to={props.url} className="nav-li-link">
         {props.name}
       </Link>
     </li>
