d514ff982bfe53db8bf1b6bb9a8a4b807a3c4b5c
Darosss <daroing12@gmail.com>
2022-11-26 18:18:13 +0100

    feat: add logs and token to send data

--- app/routes/currSessionRouter.js
+++ app/routes/currSessionRouter.js
@@ -2,11 +2,13 @@ const express = require("express");
 const router = express.Router();
 const passport = require("passport");
 router.get("/", (req, res) => {
+  console.log(req.headers.cookie, "test");
+  console.log(req.session, "kekew");
   passport.authenticate("jwt", { session: false }, (err, user) => {
     if (err || !user) {
       res.send(false);
     } else {
-      res.send(user);
+      res.send({ user, token: req.session.jwt });
     }
   })(req, res);
 });
