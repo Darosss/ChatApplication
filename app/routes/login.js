b083f5dce2519f0f99b3b916ef31f0503983ca0c
Darosss <daroing12@gmail.com>
2022-11-27 11:54:19 +0100

    refactor: remove unnecesary cookie

--- app/routes/login.js
+++ app/routes/login.js
@@ -12,14 +12,6 @@ router.post(
   authenticateUser,
   (req, res, next) => {
     console.log("Logged in as", req.user);
-    res.cookie("token", req.session.jwt, {
-      domain: "onrender.com",
-      secure: true,
-      sameSite: "none",
-      httpOnly: true,
-      expires: new Date(Date.now() + 900000),
-      httpOnly: true,
-    });
     console.log(req.session);
     console.log(res.getHeaders(), "headers");
     res.status(200).send({ token: req.session.jwt, user: req.user });
