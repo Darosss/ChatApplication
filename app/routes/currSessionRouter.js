61e14044b434bf437c87d763dfd0c8aa3b3d6ab9
Darosss <daroing12@gmail.com>
2022-11-30 20:33:00 +0100

    fix: add logs

--- app/routes/currSessionRouter.js
+++ app/routes/currSessionRouter.js
@@ -2,11 +2,8 @@ const express = require("express");
 const router = express.Router();
 const passport = require("passport");
 router.get("/", (req, res) => {
-  console.log(req.headers.cookie, "test");
-  console.log(req.headers, "headers");
-  console.log(req.session, "kekew");
+  console.log("nodeenv", process.env.NODE_ENV);
   passport.authenticate("jwt", { session: false }, (err, user) => {
-    console.log("jwt", user, "jwt err", err);
     if (err || !user) {
       res.send(false);
     } else {
