eda23e139c481d8541a22294301a2a5482387d3a
Darosss <daroing12@gmail.com>
2022-12-15 14:23:32 +0100

    refactor: change server to typescript

--- app/server.ts
+++ app/server.ts
@@ -1,23 +1,29 @@
-const app = require("./app");
-const http = require("http").Server(app);
-const mongoose = require("mongoose");
+import app from "./app";
+import http from "http";
+import mongoose, { ConnectOptions } from "mongoose";
+import { Server } from "socket.io";
+import socket from "./socket";
+const httpServer = new http.Server(app);
 
-const socketIO = require("socket.io")(http, {
+const socketIO = new Server(httpServer, {
   cors: {
     origin: process.env.FRONTEND_URL,
     credentials: true,
-    optionSuccessStatus: 200,
+    // optionSuccessStatus: 200,
     allowedHeaders:
       "X-Requested-With, content-type, x-access-token, Origin, Content-Type, Accept, Set-Cookie, Cookie",
     methods: ["GET", "POST", "DELETE"],
   },
 });
-require("./socket")(socketIO);
+socket(socketIO);
 
 //MONGODB database connection
-mongoose.connect(process.env.DATABASE_URL, { useNewUrlParser: true });
+mongoose.connect(
+  process.env.DATABASE_URL as string,
+  { useNewUrlParser: true } as ConnectOptions
+);
 
-http.listen(process.env.BACKEND_PORT || 5000, () => {
+httpServer.listen(process.env.BACKEND_PORT || 5000, () => {
   console.log(
     `application is running at: */${process.env.BACKEND_PORT || 5000}`
   );
