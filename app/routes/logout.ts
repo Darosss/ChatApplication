fd85f5eec246b153df22a543b038f5bdad08b470
Darosss <daroing12@gmail.com>
2022-12-16 11:35:14 +0100

    fix: repair logout function

--- app/routes/logout.ts
+++ app/routes/logout.ts
@@ -2,13 +2,13 @@ import express, { NextFunction, Request, Response } from "express";
 const router = express.Router();
 
 router.post("/", (req: Request, res: Response, next: NextFunction) => {
-  req.logout(function (err) {
+  req.logOut(function (err) {
     if (err) {
       return next(err);
     }
-    req.session = null;
-    res.send({ message: "Logout succes" });
   });
+  req.session = null;
+  res.send({ message: "Logout succes" });
 });
 
 export default router;
