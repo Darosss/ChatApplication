0129e5b1af91daa11bfe7306db86cee227bb6fe9
Darosss <daroing12@gmail.com>
2022-12-23 01:09:55 +0100

    feat: add types for models from database

--- frontend/src/@types/types.d.ts
+++ frontend/src/@types/types.d.ts
@@ -6,9 +6,48 @@ interface IAuth {
 }
 
 interface IChatRoomRes {
-  id?: string;
+  [_id: string]: string;
+  id: string;
+  name: string;
+  availableRanges: string[]; //TODO change to IRangeRes[]
+  allowedUsers: string[]; //TODO Change to IUserRes[]
+  bannedUsers: string[]; // TODO Change to IUserRes[]
+  createdBy: string; // TODO Change to IUserRes
 }
+
+interface IUserRes {
+  [_id: string]: string;
+  id: string;
+  username?: string;
+  firstname?: string;
+  surname?: string;
+  email?: string;
+  birthday?: Date;
+  createdAt: Date;
+  country?: string;
+  gender?: string;
+  phoneNumber?: string;
+  nickColor?: string;
+  isBanned: boolean;
+  bannedDate?: Date;
+  banExpiresDate?: Date;
+  banReason?: string;
+  ranges: IRangeRes[];
+}
+
 interface IMessagesRes {
-  id?: string;
-  roomId?: string;
+  roomId: string;
+  id: string;
+  whereSent: IChatRoomRes;
+  message: string;
+  sentTime: Date;
+  sender: IUserRes;
+}
+
+interface IRangeRes {
+  [_id: string]: string;
+  id: string;
+  name: string;
+  createdAt: Date;
+  createdBy: IUserRes;
 }
