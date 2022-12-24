04a479faecfab33b7bbee220e5097adb627b907f
Darosss <daroing12@gmail.com>
2022-12-24 11:54:27 +0100

    fix: remove unnecessary logs

--- app/routes/currSessionRouter.ts
+++ app/routes/currSessionRouter.ts
@@ -4,7 +4,6 @@ import passport from "passport";
 const router = express.Router();
 
 router.get("/", (req: Request, res: Response) => {
-  console.log("nodeenv", process.env.NODE_ENV);
   passport.authenticate("jwt", { session: false }, (err, user) => {
     if (err || !user) {
       res.send(false);
