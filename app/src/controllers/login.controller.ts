68b2aabfcd4f420e941c9524324e3151ba904e27
Darosss <daroing12@gmail.com>
2022-12-27 20:37:22 +0100

    refactor: remove checks if users is logged in condition

--- app/src/controllers/login.controller.ts
+++ app/src/controllers/login.controller.ts
@@ -2,10 +2,7 @@ import { Response } from "express";
 import { RequestUserAuth } from "@types";
 
 const login = (req: RequestUserAuth, res: Response) => {
-  if (req.user) res.send("You are already logged in");
-  else {
-    res.status(200).send({ token: req.session?.jwt, user: req.user });
-  }
+  res.status(200).send({ token: req.session?.jwt, user: req.user });
 };
 
 export { login };
