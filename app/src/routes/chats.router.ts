eabc6ac9335ff6478ea4550638ae6765a5f6ed83
Darosss <daroing12@gmail.com>
2022-12-28 23:59:02 +0100

    feat: add getRoomsMessages

--- app/src/routes/chats.router.ts
+++ app/src/routes/chats.router.ts
@@ -1,7 +1,12 @@
 import express from "express";
-import { chatsList } from "../controllers/chats.controller";
+import {
+  chatsList,
+  getRoomsMessagesById,
+} from "../controllers/chats.controller";
 const router = express.Router();
 
 router.get("/", chatsList);
+
+router.get("/:roomId", getRoomsMessagesById);
 
 export default router;
