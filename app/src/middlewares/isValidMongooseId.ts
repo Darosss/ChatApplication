a7f8c5bb5545bfccb503c3b629f0fcf2f81e90db
Darosss <daroing12@gmail.com>
2022-12-30 20:14:18 +0100

    feat: add isValidMongooseId middleware

---
+++ app/src/middlewares/isValidMongooseId.ts
@@ -0,0 +1,13 @@
+import { NextFunction, Request, Response } from "express";
+import { isValidObjectId } from "mongoose";
+
+export default function isValidMongooseId(
+  req: Request,
+  res: Response,
+  next: NextFunction
+) {
+  const { _id } = req.params;
+  if (isValidObjectId(_id)) return next();
+
+  res.send({ message: `${_id} is not proper id` });
+}
