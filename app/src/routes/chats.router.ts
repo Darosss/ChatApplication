1034e29173b757ae5872269cd7ae6901c22502d5
Darosss <daroing12@gmail.com>
2022-12-30 20:13:52 +0100

    feat: add isValidMongooseId
    change roomId to _id

--- app/src/routes/chats.router.ts
+++ app/src/routes/chats.router.ts
@@ -1,12 +1,14 @@
 import express from "express";
+import isValidMongooseId from "@/middlewares/isValidMongooseId";
 import {
   chatsList,
   getRoomsMessagesById,
 } from "../controllers/chats.controller";
+
 const router = express.Router();
 
 router.get("/", chatsList);
 
-router.get("/:roomId", getRoomsMessagesById);
+router.get("/:_id", isValidMongooseId, getRoomsMessagesById);
 
 export default router;
