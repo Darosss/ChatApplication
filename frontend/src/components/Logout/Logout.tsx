4a1cb36f521f5a0582c7e9be9c26476ebccc33d5
Darosss <daroing12@gmail.com>
2023-01-08 12:29:54 +0100

    refactor: change accios to useAcciosHook

--- frontend/src/components/Logout/Logout.tsx
+++ frontend/src/components/Logout/Logout.tsx
@@ -1,24 +1,16 @@
 import React from "react";
 import "./style.css";
-import axios from "axios";
+import useAcciosHook from "../../hooks/useAcciosHook";
 
 function Logout() {
-  const logout = () => {
-    axios({
-      method: "POST",
-      withCredentials: true,
-      url: `${process.env.REACT_APP_API_URI}/logout`,
-    })
-      .then((res) => {
-        console.log(res, "res");
-        window.location.reload();
-      })
-      .catch((err) => {
-        console.log(err, "err");
-      });
-  };
+  const { sendData: logout } = useAcciosHook({
+    url: `/logout`,
+    method: "post",
+    withCredentials: true,
+  });
+
   return (
-    <li className="">
+    <li>
       <button onClick={logout} className="nav-li-button btn btn-danger w-100">
         Logout
       </button>
