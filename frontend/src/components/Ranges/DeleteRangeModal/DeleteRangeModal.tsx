363ed39c36f2ceddb99abbb4532f10ba8cc88a7f
Darosss <daroing12@gmail.com>
2023-01-08 11:49:50 +0100

    refactor: change accios to useAcciosHook

--- frontend/src/components/Ranges/DeleteRangeModal/DeleteRangeModal.tsx
+++ frontend/src/components/Ranges/DeleteRangeModal/DeleteRangeModal.tsx
@@ -1,31 +1,28 @@
 import "./style.css";
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import ModalCore from "../../Modal";
 import axios from "axios";
+import useAcciosHook from "../../../hooks/useAcciosHook";
 
 function DeleteRangeModal(props: { rangeId: string; rangeName: string }) {
   const { rangeId, rangeName } = props;
+
   const [postInfo, setPostInfo] = useState("");
 
-  const deleteRange = () => {
-    const axiosConfig = {
-      method: "delete",
-      withCredentials: true,
-      url: `${process.env.REACT_APP_API_URI}/ranges/delete/` + rangeId,
-    };
+  const { response, sendData: deleteRange } = useAcciosHook({
+    url: `ranges/admin/delete/${rangeId}`,
+    method: "delete",
+    withCredentials: true,
+  });
 
-    axios(axiosConfig).then((res) => {
-      setPostInfo(res.data.message);
-
-      window.location.reload();
-    });
-  };
+  useEffect(() => {
+    setPostInfo(response?.data.message);
+  }, [response]);
 
   const modalBody = () => {
     return (
       <div>
         <label className="form-label "> Delete range {rangeName}</label>
-        <p className="text-danger font-weight-bold"> {postInfo} </p>
       </div>
     );
   };
