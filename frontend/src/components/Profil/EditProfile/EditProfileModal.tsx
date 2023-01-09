0da51c9025cf595bd4064c52963551d0651dca53
Darosss <daroing12@gmail.com>
2023-01-09 12:26:48 +0100

    refactor: change accios to useAcciosHook

--- frontend/src/components/Profil/EditProfile/EditProfileModal.tsx
+++ frontend/src/components/Profil/EditProfile/EditProfileModal.tsx
@@ -1,7 +1,7 @@
 import "./style.css";
 import React, { useEffect, useState } from "react";
-import axios from "axios";
 import ModalCore from "../../Modal";
+import useAcciosHook from "../../../hooks/useAcciosHook";
 
 function EditProfileModal(props: { user: IUserRes }) {
   const { user } = props;
@@ -17,6 +17,30 @@ function EditProfileModal(props: { user: IUserRes }) {
   const [nickColor, setNickColor] = useState("");
   const [postInfo, setPostInfo] = useState("");
 
+  const { response, error, sendData } = useAcciosHook({
+    url: `/profil/edit`,
+    method: "post",
+    withCredentials: true,
+    data: {
+      oldPassword: oldPassword,
+      newPassword: newPassword,
+      firstname: firstname,
+      surname: surname,
+      email: email,
+      birthday: birthday,
+      country: country,
+      phoneNumber: phone,
+      gender: gender,
+      nickColor: nickColor,
+    },
+  });
+  useEffect(() => {
+    setPostInfo(response?.data.message);
+  }, [response]);
+  useEffect(() => {
+    if (error) setPostInfo(error.message);
+  }, [error]);
+
   useEffect(() => {
     setFirstname(user.firstname as string);
     setSurname(user.surname as string);
@@ -29,28 +53,29 @@ function EditProfileModal(props: { user: IUserRes }) {
   }, [user]);
 
   const editProfile = () => {
-    const axiosEditProfile = {
-      method: "post",
-      data: {
-        oldPassword: oldPassword,
-        newPassword: newPassword,
-        firstname: firstname,
-        surname: surname,
-        email: email,
-        birthday: birthday,
-        country: country,
-        phoneNumber: phone,
-        gender: gender,
-        nickColor: nickColor,
-      },
-      withCredentials: true,
-      url: `${process.env.REACT_APP_API_URI}/profil/` + props.user._id,
-    };
-    axios(axiosEditProfile).then((res) => {
-      setPostInfo(res.data.message);
+    sendData();
+    // const axiosEditProfile = {
+    //   method: "post",
+    //   data: {
+    //     oldPassword: oldPassword,
+    //     newPassword: newPassword,
+    //     firstname: firstname,
+    //     surname: surname,
+    //     email: email,
+    //     birthday: birthday,
+    //     country: country,
+    //     phoneNumber: phone,
+    //     gender: gender,
+    //     nickColor: nickColor,
+    //   },
+    //   withCredentials: true,
+    //   url: `${process.env.REACT_APP_API_URI}/profil/` + props.user._id,
+    // };
+    // axios(axiosEditProfile).then((res) => {
+    //   setPostInfo(res.data.message);
 
-      window.location.reload();
-    });
+    //   window.location.reload();
+    // });
   };
 
   const createProfileInput = (name: string, onChangeFn: (value: any) => void, value = "", type = "text") => {
