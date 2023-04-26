00ac2adff5f7a35f138f8c9670ed5878dfe58cf5
Darosss <daroing12@gmail.com>
2023-04-27 01:56:00 +0200

    config: add aliases

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -1,6 +1,16 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
+import path from "path";
 
 export default defineConfig({
   plugins: [react()],
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src"),
+      "@libs": path.resolve(__dirname, "../libs"),
+      "@components": path.resolve(__dirname, "./src/components"),
+      "@hooks": path.resolve(__dirname, "./src/hooks"),
+      "@utils": path.resolve(__dirname, "./src/utils"),
+    },
+  },
 });
