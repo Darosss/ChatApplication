62b9afd77ff436d384ea141f5a7f2fdc97ae1ce4
Darosss <daroing12@gmail.com>
2023-04-29 13:09:58 +0200

    refactor: add new class like controllers into routes

--- app/src/routes/rooms.router.ts
+++ app/src/routes/rooms.router.ts
@@ -2,41 +2,42 @@ import { Router } from "express";
 import chatRoomValidation from "@/middlewares/chatRoomValidation";
 
 const router = Router();
-import {
-  getListOfRooms,
-  createNewRoom,
-  getRoomById,
-  editRoomById,
-  deleteRoomById,
-  getListOfUsersRooms,
-  getRoomsMessagesById,
-} from "../controllers/rooms.controller";
+import { chatRoomsController } from "../controllers/rooms.controller";
 import isValidMongooseId from "@/middlewares/isValidMongooseId";
 
-router.get("/", getListOfRooms);
+router.get("/", chatRoomsController.getListOfLoggedUserRooms);
 
 //Get list of user's rooms
-router.get("/users-rooms", getListOfUsersRooms);
+router.get("/users-rooms", chatRoomsController.getListOfUsersRooms);
 
 //Create new chatroom
-router.post("/create", createNewRoom);
+router.post("/create", chatRoomsController.createNewRoom);
 
 // Remove chatroom route
 router.delete(
   "/delete/:_id",
   isValidMongooseId,
   chatRoomValidation,
-  deleteRoomById
+  chatRoomsController.deleteRoomById
 );
 
 //Get chatroom by id
-router.get("/:_id", isValidMongooseId, getRoomById);
+router.get("/:_id", isValidMongooseId, chatRoomsController.getRoomById);
 
 //Get messages of room by id
 //TODO: add middleware to checks if user can acces or isAdmin
-router.get("/:_id/messages", isValidMongooseId, getRoomsMessagesById);
+router.get(
+  "/:_id/messages",
+  isValidMongooseId,
+  chatRoomsController.getRoomsMessagesById
+);
 
 //Edit chatroom by id route
-router.post("/edit/:_id", isValidMongooseId, chatRoomValidation, editRoomById);
+router.post(
+  "/edit/:_id",
+  isValidMongooseId,
+  chatRoomValidation,
+  chatRoomsController.editRoomById
+);
 
 export default router;
