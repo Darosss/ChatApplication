c62b3c7a4297752110125e8ac9c81af7740470de
Darosss <daroing12@gmail.com>
2023-04-29 13:08:46 +0200

    refactor(controller): change controller from functions to class methods

--- app/src/controllers/auth.controller.ts
+++ app/src/controllers/auth.controller.ts
@@ -1,57 +1,63 @@
 import { NextFunction, Request, Response } from "express";
 import { RequestUserAuth } from "@types";
-import { UserCreateData, userService } from "@/services/userService";
+import {
+  UserCreateData,
+  UserService,
+  userService,
+} from "@/services/userService";
 import passport from "passport";
 
-export const login = (req: RequestUserAuth, res: Response) => {
-  return res.status(200).send({ token: req.session?.jwt, user: req.user });
-};
+class AuthController {
+  constructor(private readonly userService: UserService) {}
 
-export const logout = (req: Request, res: Response, next: NextFunction) => {
-  req.logOut(function (err) {
-    if (err) {
-      return next(err);
-    }
-  });
-  req.session = null;
-  return res.status(200).send({ message: "Logout succes" });
-};
-
-export const register = async (
-  req: Request,
-  res: Response,
-  next: NextFunction
-) => {
-  const userData: UserCreateData = {
-    username: req.body.username,
-    password: req.body.password,
-    firstname: req.body.firstname,
-    surname: req.body.surname,
-    birthday: req.body.birthday,
-    country: req.body.country,
-    gender: req.body.gender,
-    nickColor: req.body.nickColor,
-    email: req.body.email,
-    phoneNumber: req.body.phone,
+  login = (req: RequestUserAuth, res: Response) => {
+    return res.status(200).send({ token: req.session?.jwt, user: req.user });
   };
 
-  try {
-    const message = await userService.createNewUser(
-      userData.username,
-      userData
-    );
-    return res.status(201).send(message);
-  } catch (err) {
-    return next(err);
-  }
-};
+  logout = (req: Request, res: Response, next: NextFunction) => {
+    req.logOut(function (err) {
+      if (err) {
+        return next(err);
+      }
+    });
+    req.session = null;
+    return res.status(200).send({ message: "Logout succes" });
+  };
 
-export const getSession = (req: Request, res: Response) => {
-  passport.authenticate("jwt", { session: false }, (err, user) => {
-    if (err || !user) {
-      return res.status(200).send(false);
-    } else {
-      return res.status(200).send(user);
+  register = async (req: Request, res: Response, next: NextFunction) => {
+    const userData: UserCreateData = {
+      username: req.body.username,
+      password: req.body.password,
+      firstname: req.body.firstname,
+      surname: req.body.surname,
+      birthday: req.body.birthday,
+      country: req.body.country,
+      gender: req.body.gender,
+      nickColor: req.body.nickColor,
+      email: req.body.email,
+      phoneNumber: req.body.phone,
+    };
+
+    try {
+      const message = await this.userService.createNewUser(
+        userData.username,
+        userData
+      );
+      return res.status(201).send(message);
+    } catch (err) {
+      return next(err);
     }
-  })(req, res);
-};
+  };
+
+  getSession = (req: Request, res: Response) => {
+    passport.authenticate("jwt", { session: false }, (err, user) => {
+      if (err || !user) {
+        return res.status(200).send(false);
+      } else {
+        return res.status(200).send(user);
+      }
+    })(req, res);
+  };
+}
+
+export const authController = new AuthController(userService);
