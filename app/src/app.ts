ca0b10b169bf02d329753e55aeed45d21261a1a9
Darosss <daroing12@gmail.com>
2023-04-29 13:15:41 +0200

    refactor: remove unnused routes and add auth router

--- app/src/app.ts
+++ app/src/app.ts
@@ -20,15 +20,13 @@ import userCheckBan from "@/middlewares/userCheckBan";
 import isBanned from "@/middlewares/isBanned";
 
 // ROUTES //
-import currSessionRouter from "./routes/session.router";
 import profilRouter from "./routes/profil.router";
-import loginRouter from "./routes/login.router";
-import registerRouter from "./routes/register.router";
-import logoutRouter from "./routes/logout.router";
+import authRouter from "./routes/auth.router";
 import roomsRouter from "./routes/rooms.router";
 import rangesRouter from "./routes/ranges.router";
 import usersRouter from "./routes/users.router";
 import { cookieSecret, frontendUrl, nodeEnv } from "./config/envVariables";
+import errorHandlerMiddleware from "./middlewares/errorHandler.middleware";
 
 //Security configs
 app.use(helmet());
@@ -65,13 +63,11 @@ app.use(express.urlencoded({ extended: true }));
 app.use(passport.initialize());
 app.use(passport.session());
 
-app.use("/api/v1/session", userCheckBan, currSessionRouter);
-app.use("/api/v1/login", loginRouter);
-app.use("/api/v1/register", registerRouter);
+app.use("/api/v1", userCheckBan, authRouter);
 app.use("/api/v1/profil", jwtRequired, userCheckBan, profilRouter);
-app.use("/api/v1/logout", logoutRouter);
 app.use("/api/v1/rooms", jwtRequired, isBanned, roomsRouter);
 app.use("/api/v1/ranges", jwtRequired, isBanned, rangesRouter);
 app.use("/api/v1/users", jwtRequired, isBanned, usersRouter);
 
+app.use(errorHandlerMiddleware);
 export default app;
