c16ecc9594536949b1c4eecbc27bb9a1f6cb6b95
Darosss <daroing12@gmail.com>
2023-04-29 13:09:17 +0200

    feat: add logout,register, session routes

--- app/src/routes/auth.router.ts
+++ app/src/routes/auth.router.ts
@@ -1,10 +1,12 @@
 import { Router } from "express";
-import { login } from "../controllers/login.controller";
+import { authController } from "../controllers/auth.controller";
 import authenticateUser from "@/middlewares/authenticateUser";
 import isLoggedIn from "@/middlewares/isLoggedIn";
 
 const router = Router();
 
-router.post("/", isLoggedIn, authenticateUser, login);
-
+router.post("/login", isLoggedIn, authenticateUser, authController.login);
+router.post("/logout", authController.logout);
+router.post("/register", authController.register);
+router.get("/session", authController.getSession);
 export default router;
