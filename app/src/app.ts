0edcdf17a90457eeb32a9abacf08bada0c0ce083
Darosss <daroing12@gmail.com>
2023-05-02 13:13:24 +0200

    feat: add socketIO to app.ts

--- app/src/app.ts
+++ app/src/app.ts
@@ -1,4 +1,6 @@
 import express from "express";
+import http from "http";
+import { Server } from "socket.io";
 
 const app = express();
 
@@ -6,13 +8,13 @@ import cors from "cors";
 import session from "cookie-session";
 import helmet from "helmet";
 import hpp from "hpp";
-
 import passport from "@/config/passport.strategy";
 
 import errorHandlerMiddleware from "./middlewares/errorHandler.middleware";
 import { initRoutes } from "./routes";
 import { corsOptions } from "./config/corsOptions";
 import { cookieSessionOpt } from "./config/cookieSessionOptions";
+import socket from "./socket";
 
 //Security configs
 app.use(helmet());
@@ -25,15 +27,21 @@ app.use(cors(corsOptions));
 
 // Cookie settings
 app.use(session(cookieSessionOpt));
-
 app.use(express.json()); //For JSON requests
 app.use(express.text()); // this is for plan/text format
 app.use(express.urlencoded({ extended: true }));
 
-initRoutes(app);
-
 app.use(passport.initialize());
 app.use(passport.session());
+initRoutes(app);
 
 app.use(errorHandlerMiddleware);
+
+const httpServer = new http.Server(app);
+
+const socketIO = new Server(httpServer, {
+  cors: corsOptions,
+});
+socket(socketIO);
+
 export default app;
