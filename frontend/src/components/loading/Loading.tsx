6f7ef91202d5df5207f0d60b1a2115c2976a3db1
Darosss <daroing12@gmail.com>
2023-05-03 16:26:54 +0200

    feat: add Spinner to loading screen

--- frontend/src/components/loading/Loading.tsx
+++ frontend/src/components/loading/Loading.tsx
@@ -1,9 +1,14 @@
 import React from "react";
+import { Spinner } from "react-bootstrap";
 
 function Loading() {
   return (
     <div className="section-header">
-      <h1>Loading site :)</h1>
+      <h1>
+        <Spinner animation="grow" variant="light" />
+        Loading site
+        <Spinner animation="grow" variant="light" />
+      </h1>
     </div>
   );
 }
