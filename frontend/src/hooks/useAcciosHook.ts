803113d31f7a409223c5f0701961cb7bc4909d9b
Darosss <daroing12@gmail.com>
2023-05-04 12:33:43 +0200

    make useAcciosHook generic, make sendData and fetchData async

--- frontend/src/hooks/useAcciosHook.ts
+++ frontend/src/hooks/useAcciosHook.ts
@@ -1,30 +1,25 @@
-import axios, { AxiosRequestConfig, AxiosResponse } from "axios";
+import axios, { AxiosError, AxiosRequestConfig, AxiosResponse } from "axios";
 import { useEffect, useState } from "react";
 
 axios.defaults.baseURL = import.meta.env.VITE_BACKEND_URL;
 
-interface IErrorResponseData {
-  message: string;
-  fields: string[];
-}
-
-function useAcciosHook(axiosParams: AxiosRequestConfig, refreshAfterRespond = false) {
-  const [response, setResponse] = useState<AxiosResponse>();
-  const [error, setError] = useState<IErrorResponseData>();
+function useAcciosHook<T = unknown>(axiosParams: AxiosRequestConfig, refreshAfterRespond = false) {
+  const [response, setResponse] = useState<AxiosResponse<T>>();
+  const [error, setError] = useState<AxiosError>();
   const [loading, setLoading] = useState(axiosParams.method === "GET" || axiosParams.method === "get");
 
-  function fetchData(params: AxiosRequestConfig) {
+  async function fetchData(params: AxiosRequestConfig) {
     axios(params)
       .then((res) => {
         setResponse(res);
 
         // if refreshAfterRespond refresh site
-        if (refreshAfterRespond) {
-          // add small delay
-          setTimeout(() => {
-            window.location.reload();
-          }, 100);
-        }
+        // if (refreshAfterRespond) {
+        //   // add small delay
+        //   setTimeout(() => {
+        //     window.location.reload();
+        //   }, 100);
+        // }
       })
       .catch((error) => {
         setError(error.response?.data || { message: "Something went wrong" });
@@ -34,13 +29,13 @@ function useAcciosHook(axiosParams: AxiosRequestConfig, refreshAfterRespond = fa
       });
   }
 
-  function sendData<T = unknown>(dataToSend?: T) {
+  async function sendData<T = unknown>(dataToSend?: T) {
     let params = axiosParams;
     if (dataToSend) {
       params = { ...params, data: dataToSend };
     }
 
-    fetchData(params);
+    await fetchData(params);
   }
 
   useEffect(() => {
