375bdfcaea9b0f3eb5e1cf6261b6b99547a200ff
Darosss <daroing12@gmail.com>
2023-05-04 23:20:22 +0200

    feat: add optional redirectUrl to useAcciosHook

--- frontend/src/hooks/useAcciosHook.ts
+++ frontend/src/hooks/useAcciosHook.ts
@@ -1,25 +1,23 @@
 import axios, { AxiosError, AxiosRequestConfig, AxiosResponse } from "axios";
 import { useEffect, useState } from "react";
+import { useNavigate } from "react-router-dom";
 
 axios.defaults.baseURL = import.meta.env.VITE_BACKEND_URL;
 
-function useAcciosHook<T = unknown>(axiosParams: AxiosRequestConfig, refreshAfterRespond = false) {
+function useAcciosHook<T = unknown>(axiosParams: AxiosRequestConfig, redirectUrl?: string) {
   const [response, setResponse] = useState<AxiosResponse<T>>();
   const [error, setError] = useState<AxiosError>();
   const [loading, setLoading] = useState(axiosParams.method === "GET" || axiosParams.method === "get");
+  const navigate = useNavigate();
 
   async function fetchData(params: AxiosRequestConfig) {
     axios(params)
       .then((res) => {
         setResponse(res);
 
-        // if refreshAfterRespond refresh site
-        // if (refreshAfterRespond) {
-        //   // add small delay
-        //   setTimeout(() => {
-        //     window.location.reload();
-        //   }, 100);
-        // }
+        if (redirectUrl) {
+          navigate(redirectUrl);
+        }
       })
       .catch((error) => {
         setError(error.response?.data || { message: "Something went wrong" });
