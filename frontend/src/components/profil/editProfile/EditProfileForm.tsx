912a389d5e4a399c64e1783f145f1b995b398b00
Darosss <daroing12@gmail.com>
2023-05-06 15:01:51 +0200

    feat: add validation to profil edit/create and move into separate components

---
+++ frontend/src/components/profil/editProfile/EditProfileForm.tsx
@@ -0,0 +1,133 @@
+import React from "react";
+import { Formik } from "formik";
+import { Button, Col, Form, Row } from "react-bootstrap";
+import { editProfileSchema } from "./validationSchema";
+
+interface EditProfileFormProps {
+  initialValues: LoggedUserUpdateData;
+  onSubmit: (values: LoggedUserUpdateData) => void;
+}
+
+function EditProfileForm(props: EditProfileFormProps) {
+  const { initialValues, onSubmit } = props;
+  return (
+    <Formik
+      validationSchema={editProfileSchema}
+      initialValues={initialValues}
+      onSubmit={(values, actions) => {
+        console.log("test");
+        onSubmit(values);
+        actions.setSubmitting(false);
+      }}
+    >
+      {({ handleSubmit, handleChange, values, touched, errors }) => (
+        <Form onSubmit={handleSubmit} className="w-75" noValidate>
+          <Form.Group as={Row} className="mt-5">
+            <Form.Group as={Col} controlId="validation-oldPassword">
+              <Form.Label>Old password</Form.Label>
+              <Form.Control
+                name="oldPassword"
+                type="password"
+                autoComplete="current-password"
+                value={values.oldPassword}
+                onChange={handleChange}
+                isValid={touched.oldPassword && !errors.oldPassword}
+                isInvalid={!!errors.oldPassword}
+              />
+            </Form.Group>
+            <Form.Group as={Col} controlId="validation-newPassword">
+              <Form.Label>New password</Form.Label>
+              <Form.Control
+                name="newPassword"
+                type="password"
+                autoComplete="new-password"
+                value={values.newPassword}
+                onChange={handleChange}
+                isValid={touched.newPassword && !errors.newPassword}
+                isInvalid={!!errors.newPassword}
+              />
+              <Form.Control.Feedback>Looks good!</Form.Control.Feedback>
+              <Form.Control.Feedback type="invalid">{errors.newPassword}</Form.Control.Feedback>
+            </Form.Group>
+          </Form.Group>
+          <Form.Group as={Row} className="mt-5">
+            <Form.Group as={Col}>
+              <Form.Label>Firstname</Form.Label>
+              <Form.Control name="firstname" type="text" value={values.firstname} onChange={handleChange} />
+            </Form.Group>
+            <Form.Group as={Col}>
+              <Form.Label>Surname</Form.Label>
+              <Form.Control name="surname" type="text" value={values.surname} onChange={handleChange} />
+            </Form.Group>
+          </Form.Group>
+
+          <Form.Group as={Col} controlId="validation-email">
+            <Form.Label>E-mail</Form.Label>
+            <Form.Control
+              name="email"
+              type="email"
+              value={values.email}
+              onChange={handleChange}
+              isValid={touched.email && !errors.email}
+              isInvalid={!!errors.email}
+            />
+            <Form.Control.Feedback>Looks good!</Form.Control.Feedback>
+            <Form.Control.Feedback type="invalid">{errors.email}</Form.Control.Feedback>
+          </Form.Group>
+
+          <Form.Group as={Col} controlId="validation-birthday">
+            <Form.Label>Birthday</Form.Label>
+            <Form.Control
+              name="birthday"
+              type="date"
+              value={values.birthday.toString().slice(0, 10)}
+              onChange={handleChange}
+              isValid={touched.birthday && !errors.birthday}
+              isInvalid={!!errors.birthday}
+            />
+            <Form.Control.Feedback>Looks good!</Form.Control.Feedback>
+            <Form.Control.Feedback type="invalid">
+              {!errors.birthday ? null : String(errors.birthday || "")}
+            </Form.Control.Feedback>
+          </Form.Group>
+
+          <Form.Group as={Row} className="mt-5">
+            <Form.Group as={Col}>
+              <Form.Label>Country</Form.Label>
+              <Form.Control name="country" type="text" value={values.country} onChange={handleChange} />
+            </Form.Group>
+            <Form.Group as={Col}>
+              <Form.Label>Gender</Form.Label>
+              <Form.Control name="gender" type="text" value={values.gender} onChange={handleChange} />
+            </Form.Group>
+            <Form.Group as={Col}>
+              <Form.Label>Nick color</Form.Label>
+              <Form.Control name="nickColor" type="text" value={values.nickColor} onChange={handleChange} />
+            </Form.Group>
+          </Form.Group>
+
+          <Form.Group as={Col} controlId="validation-phoneNumber">
+            <Form.Label>Phone</Form.Label>
+            <Form.Control
+              name="phoneNumber"
+              type="text"
+              value={values.phoneNumber}
+              onChange={handleChange}
+              isValid={touched.phoneNumber && !errors.phoneNumber}
+              isInvalid={!!errors.phoneNumber}
+            />
+            <Form.Control.Feedback>Looks good!</Form.Control.Feedback>
+            <Form.Control.Feedback type="invalid">{errors.phoneNumber}</Form.Control.Feedback>
+          </Form.Group>
+          <Form.Group as={Col} className="m-5">
+            <Button type="submit" className="btn btn-primary w-100">
+              Edit
+            </Button>
+          </Form.Group>
+        </Form>
+      )}
+    </Formik>
+  );
+}
+
+export default EditProfileForm;
--- frontend/src/components/profil/editProfile/EditProfileModal.tsx
+++ frontend/src/components/profil/editProfile/EditProfileModal.tsx
@@ -4,117 +4,55 @@ import usePostInfoHook from "@hooks/usePostInfoHook";
 import { SendDataContext } from "@contexts/SendDataContext";
 import { useProfilEdit } from "@hooks/profilApi";
 import { useRefetchData } from "@hooks/useAcciosHook";
+import EditProfileForm from "./EditProfileForm";
+import PostInfo from "@components/postInfo";
 
 function EditProfileModal(props: { user: IUserRes }) {
   const { user } = props;
 
   const { sendData: refetchData } = useContext(SendDataContext);
 
-  const [oldPassword, setOldPassword] = useState("");
-  const [newPassword, setNewPassword] = useState("");
-  const [firstname, setFirstname] = useState("");
-  const [surname, setSurname] = useState("");
-  const [email, setEmail] = useState("");
-  const [birthday, setBirthday] = useState<Date>(new Date());
-  const [country, setCountry] = useState("");
-  const [phone, setPhone] = useState("");
-  const [gender, setGender] = useState("");
-  const [nickColor, setNickColor] = useState("");
-
-  const { profilEditResponse, profilEditError, profilEdit } = useProfilEdit({
-    oldPassword: oldPassword,
-    newPassword: newPassword,
-    firstname: firstname,
-    surname: surname,
-    email: email,
-    birthday: birthday,
-    country: country,
-    phoneNumber: phone,
-    gender: gender,
-    nickColor: nickColor,
+  const [profilValues, setProfilValues] = useState<LoggedUserUpdateData>({
+    oldPassword: "",
+    newPassword: "",
+    firstname: "",
+    surname: "",
+    birthday: new Date(),
+    country: "",
+    gender: "",
+    nickColor: "",
+    email: "",
+    phoneNumber: "",
   });
 
+  const { profilEditResponse, profilEditError, profilEdit } = useProfilEdit();
+
   useEffect(() => {
-    setFirstname(user.firstname as string);
-    setSurname(user.surname as string);
-    setEmail(user.email as string);
-    setBirthday(user.birthday);
-    setCountry(user.country as string);
-    setPhone(user.phoneNumber as string);
-    setGender(user.gender as string);
-    setNickColor(user.nickColor as string);
+    setProfilValues({
+      oldPassword: "",
+      newPassword: "",
+      firstname: user.firstname,
+      surname: user.surname,
+      birthday: user.birthday,
+      country: user.country,
+      gender: user.gender,
+      nickColor: user.nickColor,
+      email: user.email,
+      phoneNumber: user.phoneNumber,
+    });
   }, [user]);
 
   const { postInfo } = usePostInfoHook(profilEditResponse?.data.message, profilEditError?.message);
 
   useRefetchData(profilEditResponse, refetchData);
 
-  const handleOnClickEditProfile = () => {
-    profilEdit();
-  };
-
-  const createProfileInput = (name: string, onChangeFn: (value: any) => void, value = "", type = "text") => {
-    return (
-      <input
-        name={name}
-        type={type}
-        className="form-control"
-        value={value}
-        onChange={(e) => onChangeFn(e.target.value)}
-      />
-    );
-  };
-  const createTwoInputGroup = (labelName: string, firstInput: JSX.Element, secondInput: JSX.Element) => {
-    return (
-      <>
-        <label className="form-label">{labelName}</label>
-        <div className="input-group">
-          {firstInput}
-          {secondInput}
-        </div>
-      </>
-    );
-  };
-
-  const modalBody = () => {
-    return (
-      <div>
-        {createTwoInputGroup(
-          "Old password / New password",
-          createProfileInput("oldPassword", setOldPassword, oldPassword, "password"),
-          createProfileInput("newPassword", setNewPassword, newPassword, "password"),
-        )}
-        {createTwoInputGroup(
-          "Firstname / Surname",
-          createProfileInput("firstname", setFirstname, firstname),
-          createProfileInput("surname", setSurname, surname),
-        )}
-
-        <label className="form-label">Email</label>
-        {createProfileInput("email", setEmail, email)}
-        <label className="form-label">Birthday</label>
-        {createProfileInput("birthday", setBirthday, birthday ? birthday?.toString().split("T")[0] : "", "date")}
-        <label className="form-label">Country</label>
-        {createProfileInput("country", setCountry, country)}
-        <label className="form-label">Gender</label>
-        {createProfileInput("gender", setGender, gender)}
-        <label className="form-label">Phone</label>
-        {createProfileInput("phone", setPhone, phone)}
-        <label className="form-label">Nick color</label>
-        {createProfileInput("nickColor", setNickColor, nickColor)}
-      </div>
-    );
-  };
-
   return (
-    <ModalCore
-      actionName="Edit"
-      onClickFn={handleOnClickEditProfile}
-      actionBtnVariant="primary"
-      postInfo={postInfo}
-      closeOnSubmit={true}
-    >
-      {modalBody()}
+    <ModalCore actionName="Edit" actionBtnVariant="primary" postInfo={postInfo} form={true}>
+      <div className="d-flex justify-content-center">
+        <EditProfileForm initialValues={profilValues} onSubmit={profilEdit<LoggedUserUpdateData>} />
+      </div>
+
+      <PostInfo info={postInfo} />
     </ModalCore>
   );
 }
