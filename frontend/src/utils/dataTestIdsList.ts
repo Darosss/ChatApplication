7a9698feae57093a885ee23075c038bf49d18894
Darosss <daroing12@gmail.com>
2023-05-20 23:39:58 +0200

    test: add test ids in utils file

---
+++ frontend/src/utils/dataTestIdsList.ts
@@ -0,0 +1,121 @@
+export const homeElementId = "home-element";
+export const loginlementId = "login-element";
+export const registerElementId = "register-element";
+export const chatsElementId = "chats-element";
+export const profilElementId = "profil-element";
+export const roomsElementId = "rooms-element";
+export const usersElementId = "users-element";
+export const rangesElementId = "ranges-element";
+export const loadingElementId = "loading-element";
+export const logoutElementId = "logout-element";
+export const postInfoElementId = "post-info-element";
+export const navigationBarElementId = "navigation-bar-element";
+export const navigationBarIds = {
+  home: "home-link",
+  chats: "chats-link",
+  login: "login-link",
+  register: "register-link",
+  rooms: "rooms-link",
+  profil: "profil-link",
+  users: "users-link",
+  ranges: "ranges-link",
+};
+export const infoSidebarIds = {
+  toggleBtn: "toggle-btn-info-sidebar-element",
+  infoSidebar: "info-sidebar-element",
+};
+
+export const modalIds = {
+  toggleBtn: "toggle-btn-modal-element",
+  modal: "modal-element",
+};
+
+export const loginFormIds = {
+  form: "login-form-element",
+  usernameInput: "username-input-login",
+  passwordInput: "password-input-login",
+};
+
+export const registerFormIds = {
+  form: "register-form-element",
+  usernameInput: "username-input-register",
+  passwordInput: "password-input-register",
+  emailInput: "email-input-register",
+  firstnameInput: "firstname-input-register",
+  surnameInput: "surname-input-register",
+  birthdayInput: "birthday-input-register",
+  phoneInput: "phone-input-register",
+  countryInput: "country-input-register",
+  genderInput: "gender-input-register",
+  nickColorInput: "nickColor-input-register",
+};
+
+export const usersIds = {
+  banUserModal: {
+    banTimeInput: "ban-time-input-users",
+    banReasonInput: "ban-reason-input-users",
+    banSubmitButton: "ban-submit-users",
+  },
+  unbanUserModal: {
+    unbanSubmitButton: "unban-submit-users",
+  },
+  editUserModal: {
+    form: "user-edit-form-element",
+    usernameInput: "username-input-edit",
+    passwordInput: "password-input-edit",
+    emailInput: "email-input-edit",
+    firstnameInput: "firstname-input-edit",
+    surnameInput: "surname-input-edit",
+    birthdayInput: "birthday-input-edit",
+    phoneInput: "phone-input-edit",
+    countryInput: "country-input-edit",
+    genderInput: "gender-input-edit",
+    nickColorInput: "nickColor-input-edit",
+    rangesSelect: "ranges-select-edit",
+  },
+};
+
+export const roomsIds = {
+  deleteRoomModal: {
+    roomDeleteSubmitButton: "delete-submit-room",
+  },
+  editCreateModal: {
+    form: "room-form-element",
+    nameInput: "name-input-room",
+    availableRanges: "available-ranges-select-room",
+    allowedUsers: "allowed-users-select-room",
+    bannedUsers: "banned-users-select-room",
+  },
+};
+
+export const rangesIds = {
+  deleteRangeModal: {
+    rangeDeleteSubmitButton: "delete-submit-room",
+  },
+  editCreateModal: {
+    form: "range-form-element",
+    nameInput: "name-input-range",
+  },
+};
+
+export const profilIds = {
+  editProfile: {
+    form: "profile-form-element",
+    oldPasswordInput: "old-password-input-profile",
+    newPasswordInput: "new-password-input-profile",
+    firstnameInput: "firstname-input-profile",
+    surnameInput: "surname-input-profile",
+    birthdayInput: "birthday-input-profile",
+    countryInput: "country-input-profile",
+    genderInput: "gender-input-profile",
+    nickColorInput: "nick-color-input-profile",
+    emailInput: "email-input-profile",
+    phoneInput: "phone-input-profile",
+  },
+};
+
+export const chatsIds = {
+  chatRoom: "chat-room-element",
+  onlineUsersBtnToggle: "chat-room-online-users-btn",
+  sendMessageBtn: "chat-room-send-message-btn",
+};
